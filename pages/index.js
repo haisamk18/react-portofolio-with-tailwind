6814f9eb35ba2aa61a7a3a88b37f3f90b8ea4e28
haisamk18 <khurshidhaisam@gmail.com>
2023-06-04 00:35:54 +0530

    ..

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import {
-  AiFillTwitterCircle,
+  AiFillInstagram,
   AiFillLinkedin,
-  AiFillYoutube,
+ 
+  AiFillGithub
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/dev-ed-wave.png";
+import Img from "../public/Img.jpg";
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
@@ -31,7 +32,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">developedbyhk</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -51,22 +52,22 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Haisam Khurshid
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              Full Stack Developer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Freelancer providing services for programming and design content
               needs. Join me down below and let's get cracking!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
+              <a href="https://github.com/haisamk18" target='_blank'><AiFillGithub /></a>
+              <a href="https://www.linkedin.com/in/haisam-khurshid/" target='_blank'><AiFillLinkedin /></a>
+              <a href="https://www.instagram.com/haixsam/" target='_blank'><AiFillInstagram /></a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
+              <Image src={Img} layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>
